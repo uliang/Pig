f242d6a203fd4eed70da42fce876d5b1b95bfadd
Tang U-Liang <uliang6482@gmail.com>
2020-10-05 15:42:11 +0800

    Update constants.py
    added more constants

--- pig/game/constants.py
+++ pig/game/constants.py
@@ -2,6 +2,8 @@ from enum import Enum
 
 
 class states(Enum): 
-    END_TURN = 0
-    CONTINUE = 1
-    EXIT=2
+    END_TURN    = 0
+    CONTINUE    = 1
+    EXIT        = 2
+    NPC         = 3
+    PC          = 4
