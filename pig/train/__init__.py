a5ff81e8a9dcfae5cd5bbd4dc0dd7715acd2ba76
Tang U-Liang <uliang6482@gmail.com>
2020-10-08 17:27:27 +0800

    cast to matrix form

--- pig/train/__init__.py
+++ pig/train/__init__.py
@@ -1,15 +1,32 @@
+import math
 import numpy as np 
 from numpy.random import rand 
 
 from tqdm import tqdm
 
-def indexer(size): 
+from .array_utils import set_ones, set_b 
 
-    for i,j in zip(*(reversed(id_arr) 
-                    for id_arr in np.triu_indices(size))):
-        yield i,j 
+def indexer(size): 
+    """
+    indexer ensures that indices of the array are partitioned and yielded
+    according to their total scores. The intended sequence of index pairs
+    are
+        (99,99), (98,99), (97,99), (98,98), (96,99), (97,98), ...
+    """
+    for s_part in range(198,-1, -1):
+        for i in range(s_part-99, math.ceil((s_part+1)/2)):
+            yield i, s_part-i 
 
 def training_loop(max_iter=10, tol=0.001): 
+    """
+    The estimated value for each action is:
+    'hold' = V(s'), 
+    'roll' = \sum_s' 1/6 * ( R_ss' + V(s')) where the reward for holding
+    is always 0 since you can never transition from a non-winning state
+    to a winning state by holding. 
+
+    R_ss' = 1 iff s -> s' is a transition from nonwinning to winning.
+    """ 
     P = rand(100, 100, 100)
 
     score_partition = list(indexer(100))
@@ -17,47 +34,33 @@ def training_loop(max_iter=10, tol=0.001):
     bar_fmt = '{elapsed}|{bar}|{percentage:.0f}% [{rate_fmt}]'
     with tqdm(total=len(score_partition), ncols=60, unit_scale=1, 
               bar_format=bar_fmt, leave=False) as t:
-        """
-        The estimated value for each action is:
-        'hold' = V(s'), 
-        'roll' = \sum_s' 1/6 * ( R_ss' + V(s')) where the reward for holding
-        is always 0 since you can never transition from a non-winning state
-        to a winning state by holding. 
-
-        R_ss' = 1 iff s -> s' is a transition from nonwinning to winning.
-        """ 
-        for I, J in score_partition:   
+       for i, j in score_partition:   
             delta = 0 
-            for _ in range(max_iter): 
-                i,j = I,J
-                switched = 0 
 
-                while switched < 2: 
-                    for k in range(100-i): 
-                        v = P[i,j,k]
-
-                        roll = 1 - P[j,i,0]                             # rolled a 1. 
-                        
-                        for m in range(2,7): 
-                            roll += P[i,j, k+m] if m < 100-i-k else 1   # reward only transition to winning states
-
-                        roll /= 6
-                        
-                        hold = 1 - P[j, i+k, 0] if i+k < 100 else 0     # So that the AI learns to hold in a 
-                                                                        # nonwinning state
-                        P[i, j, k] = max(hold, roll) 
-                    
-                        delta = max(np.abs(v-P[i,j,k]), delta)
-
-                    if i != j:
-                        i,j = J,I
-                        switched += 1 
-                    else:
-                        break 
-
-                if delta < tol:
-                    break
+            v = np.r_[P[i, j, 0:100-i], P[j, i, 0:100-j]].T
             
+            b = np.r_[set_b(100-i), set_b(100-j)].T
+
+            hold = np.array([1-P[j, _i, 0] for _i in range(i,100)] 
+                              + [1-P[i, _j, 0] for _j in range(j,100)]).T 
+
+            A = np.c_[[set_ones(100-i, k+2, k+7) for k in range(100-i)]]
+            B = np.c_[[-1*set_ones(100-j, 0, 1) for k in range(100-i)]]
+            C = np.c_[[-1*set_ones(100-i, 0, 1) for k in range(100-j)]]
+            D = np.c_[[set_ones(100-j, k+2, k+7) for k in range(100-j)]] 
+
+            M = np.matrix(np.c_[np.r_[A, B], np.r_[C, D]])
+
+            v_temp = np.maximum(M @ v + b, hold).T
+            
+            P[i, j, 0:100-i] = v_temp[0:100-i]
+            P[j, i, 0:100-j] = v_temp[100-i:]
+
+            delta = np.abs(v-v_temp).max()
+
+            if delta < tol:
+                break
+        
             t.update(1)
 
     return P
